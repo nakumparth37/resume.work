ee9e2e19c87586375d775e7956592257f2fab5ef
Parth Nakum <75476666+nakumparth37@users.noreply.github.com>
2023-02-04 19:30:31 +0530

    uplord  the forms

---
+++ employee details form/get_form_info.js
@@ -0,0 +1,22 @@
+function get_info() {
+    var name = document.getElementById("E_Name").value;
+    var age = document.getElementById("E_age").value;
+    var designation = document.getElementById("designation").value;
+    var salary = document.getElementById("salary").value;
+    var E_location = document.getElementById("location_list").value;
+    var email_id = document.getElementById("email_id").value;
+    var Date = document.getElementById("date_of_joining").value;
+    var contact = document.getElementById("contact").value;
+
+    // console.log(name);
+    var e_name = localStorage.setItem("name", name);
+    var e_age = localStorage.setItem("age", age);
+    var e_designation = localStorage.setItem("designation", designation);
+    var e_salary = localStorage.setItem("salary", salary);
+    var E_location = localStorage.setItem("E_location", E_location);
+    var e_email = localStorage.setItem("email_id ", email_id);
+    var e_Date = localStorage.setItem("Date ", Date);
+    var e_contact = localStorage.setItem("contact ", contact)
+
+}
+
---
+++ employee details form/vaildastion.js
@@ -0,0 +1,57 @@
+function validateForm() {
+    let name = document.forms["Employee_detials"]["Employee_Name"].value;
+    if (name == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+
+    let age = document.forms["Employee_detials"]["Employee_Age"].value;
+    if (age == "") {
+      alert("Age must be filled out");
+      return false;
+    }
+
+    let gender = document.forms["Employee_detials"]["Employee_Gender"].value;
+    if (gender == "") {
+      alert("Gender must be selected");
+      return false;
+    }
+
+    let location = document.forms["Employee_detials"][""].value;
+    if (location == "") {
+      alert("Location must be filled out or selected");
+      return false;
+    }
+    // else if (isNaN(location) == false) {
+    //   alert("Location  can't be number");
+    // }
+
+    let email = document.forms["Employee_detials"]["Employee_Email"].value;
+    var petten = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
+    if (email == "") {
+      alert("email must be filled out");
+      return false;
+    }
+    else if (match(petten) != email) {
+      alert("email must be in frometted");
+    }
+
+    let date = document.forms["Employee_detials"]["Date_of_Joining"].value;
+    if (date == "") {
+      alert("data must be filled out");
+      return false;
+    }
+
+    let contact = document.forms["Employee_detials"]["Contect"].value;
+    if (contact == "") {
+      alert("Contect number must be filled out");
+      return false;
+    }
+    else if (contect.length == 10) {
+      alert("Contect number must be 10 Digit");
+      return false;
+    } 
+
+    
+    
+  }
