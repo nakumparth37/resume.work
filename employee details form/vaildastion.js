008ca418c03077a7e493a98a2a61626ecaab6d27
nakumparth37 <nakumparth37@gmail.com>
2023-02-06 10:41:47 +0530

    make some cheng in vaildestion for email

--- employee details form/vaildastion.js
+++ employee details form/vaildastion.js
@@ -1,69 +1,73 @@
 function validateForm() {
-    let name = document.forms["Employee_detials"]["Employee_Name"].value;
-    if (name == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-
-    let age = document.forms["Employee_detials"]["Employee_Age"].value;
-    if (age == "") {
-      alert("Age must be filled out");
-      return false;
-    }
-
-    let gender = document.forms["Employee_detials"]["Employee_Gender"].value;
-    if (gender == "") {
-      alert("Gender must be selected");
-      return false;
-    }
-
-    let designation = document.forms["Employee_detials"]["designation"].value;
-    if (designation == "") {
-      alert("designation must be selected");
-      return false;
-    }
-
-    let location = document.forms["Employee_detials"]["location"].value;
-    if (location == "") {
-      alert("Location must be filled out or selected");
-      return false;
-    }
-    // else if (isNaN(location) == true) {
-    //   alert("Location  can't be number");
-    // }
-
-    let salary = document.forms["Employee_detials"]["salary"].value;
-    if (salary == "") {
-      alert("salary must be filled out");
-      return false;
-    }
-
-    let email = document.forms["Employee_detials"]["Employee_Email"].value;
-    // var petten = "^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$";
-    if (email == "") {
-      alert("email must be filled out");
-      return false;
-    }
-    // else if (petten.match(email)) {
-    //   alert("email must be in frometted");
-    // }
-
-    let date = document.forms["Employee_detials"]["Date_of_Joining"].value;
-    if (date == "") {
-      alert("data must be filled out");
-      return false;
-    }
-
-    let contact = document.getElementById("contact").value;
-    if (contact == "") {
-      alert("Contect number must be filled out");
-      return false;
-    }
-     else if (contect.length == 10) {
-      alert("Contect number must be 10 Digit");
-      return false;
-    } 
-
-    
-    
+  let name = document.forms["Employee_detials"]["Employee_Name"].value;
+  if (name == "") {
+    alert("Name must be filled out");
+    return false;
   }
+
+  let age = document.forms["Employee_detials"]["Employee_Age"].value;
+  if (age == "") {
+    alert("Age must be filled out");
+    return false;
+  }
+
+  let gender = document.forms["Employee_detials"]["Employee_Gender"].value;
+  if (gender == "") {
+    alert("Gender must be selected");
+    return false;
+  }
+
+  let designation = document.forms["Employee_detials"]["designation"].value;
+  if (designation == "") {
+    alert("designation must be selected");
+    return false;
+  }
+
+  let location = document.forms["Employee_detials"]["location"].value;
+  // let location_petten = /^[a-zA-Z+|[0-9]\s]+$/;
+  if (location == "") {
+    alert("Location must be filled out or selected");
+    return false;
+  }
+  // else if (!location_petten.test(location)) {
+  //   alert("Location can not have number ");
+  //   return false;
+  // }
+
+
+  let salary = document.forms["Employee_detials"]["salary"].value;
+  if (salary == "") {
+    alert("salary must be filled out");
+    return false;
+  }
+
+  let email = document.forms["Employee_detials"]["Employee_Email"].value;
+  var email_petten = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/g;
+  if (email == "") {
+    alert("email must be filled out");
+    return false;
+  }
+  else if (!email_petten.test(email)) {
+    alert("Email is not vild");
+    return false;
+  }
+
+  let date = document.forms["Employee_detials"]["Date_of_Joining"].value;
+  if (date == "") {
+    alert("data must be filled out");
+    return false;
+  }
+
+  let contact = document.getElementById("contact").value;
+  if (contact == "") {
+    alert("Contect number must be filled out");
+    return false;
+  }
+  else if (contact.length != 10) {
+    alert("Contect number must be 10 Digit");
+    return false;
+  }
+
+
+
+}
